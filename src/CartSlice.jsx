929162469bc8e8b950ecda3b1f155e7a7279cecc
Samukelisiwe Mazibuko <mazibukosamukelisiwe75@gmail.com.za>
2026-05-15 11:29:46 -0400

    ...

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -2,37 +2,43 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
   name: 'cart',
+
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
+
   reducers: {
     addItem: (state, action) => {
-        const existingItem = state.items.find(
-            (item) => item.name === action.payload.name
-        );
+      const existingItem = state.items.find(
+        (item) => item.name === action.payload.name
+      );
 
-        if (existingItem) {
-            existingItem.quantity += 1;
-        } else {
-            state.items.push({ ...action.payload, quantity: 1 });
-        }
-    
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
     },
+
     removeItem: (state, action) => {
-        state.items = state.items.filter(
-            (item) => item.name !== action.payload
-        );
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload
+      );
     },
+
     updateQuantity: (state, action) => {
-        const { name, amount } = action.payload;
+      const { name, amount } = action.payload;
 
-        const itemToUpdate = state.items.find(
-            (item) => item.name === name
-        );
+      const itemToUpdate = state.items.find(
+        (item) => item.name === name
+      );
 
-        if (itemToUpdate) {
-            itemToUpdate.quantity = amount;
-        }
+      if (itemToUpdate) {
+        itemToUpdate.quantity = amount;
+      }
     },
   },
 });
